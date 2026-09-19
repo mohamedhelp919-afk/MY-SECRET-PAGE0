63084e05821b8a9da097f3241e741caff779c0cb
mohamedhelp919-afk <mohamedhelp919@gmail.com>
2026-09-20 01:50:11 +0300

    Delete Net.sql

--- Net.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE orders (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    order_code VARCHAR(20) NOT NULL,
-    amount DECIMAL(10,2) NOT NULL,
-    phone_number VARCHAR(15) NOT NULL,
-    status ENUM('pending', 'paid', 'cancelled') DEFAULT 'pending',
-    card_code VARCHAR(100) DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
